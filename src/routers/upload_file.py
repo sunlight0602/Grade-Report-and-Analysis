5ed1f4064938ccfbbe9c0cbb3da20fb8011791dd
kyshen <shensunny594@gmail.com>
2024-07-13 01:15:43 +0800

    feat: process file after upload

--- src/routers/upload_file.py
+++ src/routers/upload_file.py
@@ -1,21 +1,16 @@
 """handle file upload"""
 
-import io
-
-import openpyxl
-import pandas as pd
 from fastapi import APIRouter, File, HTTPException, UploadFile
 from fastapi.responses import JSONResponse
 
+from src.GradeReportAndAnalysis.cwt_student_report import CWTStudentReport
+from src.GradeReportAndAnalysis.cwt_teacher_report import CWTTeacherReport
 from src.GradeReportAndAnalysis.info import Info
-# from ..GradeReportAndAnalysis.info import Info
-
-# from src.GradeReportAndAnalysis.info import Info
+from src.GradeReportAndAnalysis.rank import Rank
 
 router = APIRouter()
 
 
-# @app.post("/uploadfile/")
 @router.post("/uploadfile/")
 async def upload_file(file: UploadFile = File(...)):
     """process excel file and pass to database"""
@@ -24,25 +19,24 @@ async def upload_file(file: UploadFile = File(...)):
             status_code=400, detail="Invalid file type. Only .xlsx files are allowed."
         )
 
-    # Read the file content
     content = await file.read()
-    workbook = openpyxl.load_workbook(io.BytesIO(content))
-    sheet = workbook.active
-
-    # Another read file content
-    # pages = pd.read_excel(
-    #     io=content,
-    #     sheet_name=None,
-    #     keep_default_na=False,
-    # )
     info = Info(file_name="", excel_file=content, from_exe=False)
-
-    # Process the data (example: print all rows)
-    data = []
-    for row in sheet.iter_rows(values_only=True):
-        data.append(row)
-        print(row)  # Example action: print each row
+    names = [student.name for student in info.students]
 
     return JSONResponse(
-        content={"message": "File processed successfully", "data": data}
+        content={"message": "File processed successfully", "data": names}
     )
+
+
+def process_file(info: Info):
+    """process info"""
+
+    rank = Rank(info.students)
+    rank.calculate_rank()
+
+    for student in info.students:
+        cwt_student_report = CWTStudentReport(student, info, rank)
+        cwt_student_report.generate_student_report()
+
+    cwt_teacher_report = CWTTeacherReport(student, info, rank)
+    cwt_teacher_report.generate_teacher_report()
